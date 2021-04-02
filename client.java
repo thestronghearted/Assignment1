9642f0ea0e36a140a667421437e0c1d888899868
Cameron Misplon <xdeecsgo@gmail.com>
2021-04-02 21:48:43 +0200

    Now echoes messages with udp protocol

--- client.java
+++ client.java
@@ -5,22 +5,31 @@ import java.util.*;
 public class client{
 	public static void main(String[] args){
 		int port = 32517;
-		try(Socket tcpsocket = new Socket(InetAddress.getLocalHost().getHostName(),port))
+		byte[] message;
+		String echo;
+		byte[] ipAddr = new byte[]{127,0,1,1};
+		try(Socket tcpsocket = new Socket(InetAddress.getByAddress(ipAddr),port))
 		{
-			OutputStream output = tcpsocket.getOutputStream();
-			PrintWriter writer = new PrintWriter(output, true);
+			InputStream tcpinput = tcpsocket.getInputStream();
+			BufferedReader tcpreader = new BufferedReader(new InputStreamReader(tcpinput));
+			System.out.println(tcpreader.readLine());
+			InetAddress serveraddress = tcpsocket.getInetAddress();
+			DatagramSocket udpsocket = new DatagramSocket();
 			Console input = System.console();
 			String text;
+			tcpsocket.close();
 			do{
 				text = input.readLine("Enter text: ");
-				writer.println(text);
-				InputStream in = tcpsocket.getInputStream();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-				String response = reader.readLine();
-				System.out.println(response);
+				message = text.getBytes();
+				DatagramPacket packet = new DatagramPacket(message, message.length, serveraddress,port);
+				udpsocket.send(packet);
+				packet = new DatagramPacket(message,message.length);
+				udpsocket.receive(packet);
+				echo = new String(packet.getData(), 0, packet.getLength());
+				System.out.println(echo);
 			}
-			while (!text.equals("bye"));
-			tcpsocket.close();
+			while (!echo.equals("bye"));
+			udpsocket.close();
 		}
 		catch (UnknownHostException e){
 			e.printStackTrace();
--- server.java
+++ server.java
@@ -4,27 +4,33 @@ import java.util.*;
 
 public class server {
 	public static void main(String[] args){
-		DatagramSocket[] udpsocket;
+		DatagramSocket udpsocket;
 		Socket tcpsocket;
-		String message;
+		byte[] message = new byte[256];
 		int port = 32517;
+		String messagereceived;
 		try (ServerSocket serverSocket = new ServerSocket(port)){
 			System.out.println("Server is up on 127.0.1.1 listening to port 32517");
 			while(true){
 				tcpsocket  = serverSocket.accept();
-				System.out.println("New Client");
-				InputStream input = tcpsocket.getInputStream();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-				OutputStream output = tcpsocket.getOutputStream();
-				PrintWriter writer = new PrintWriter(output, true);
-
-				do {
-					message = reader.readLine();
-					String text = new StringBuilder(message).reverse().toString();
-					writer.println("Server: " + text);
-				}
-				while(!message.equals("bye"));
+				OutputStream tcpoutput = tcpsocket.getOutputStream();
+				PrintWriter tcpwriter = new PrintWriter(tcpoutput, true);
+				tcpwriter.println("You are connected");
 				tcpsocket.close();
+				udpsocket = new DatagramSocket(port);
+				do {
+					DatagramPacket packet = new DatagramPacket(message, message.length);
+					udpsocket.receive(packet);
+					InetAddress useraddress = packet.getAddress();
+					int userport = packet.getPort();
+					messagereceived = new String(packet.getData(),0,packet.getLength());
+					message = messagereceived.getBytes();
+					packet = new DatagramPacket(message,message.length,useraddress,userport);
+					udpsocket.send(packet);
+					message = new byte[256];
+				}
+				while(!messagereceived.equals("bye"));
+				udpsocket.close();
 			}
 		}
 		catch (IOException e){
